07780b3eb95809ab81ce44c59e4819207de2b496
Lucas <ljrodriguez1@uc.cl>
2020-11-06 12:37:18 -0300

    pls

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ api.get('/groups', function (req, res) {
 
 router.get('/', function (req, res) {
   console.log('holaaaa')
-  res.send('hola como esta Updated ultima10');
+  res.send('hola como esta Updated ultima11');
 });
 
 router.get('/world', function (req, res) {
