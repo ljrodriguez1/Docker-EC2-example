3c630e2d620563d4bf01a5fe27478589f8875ce8
Lucas <ljrodriguez1@uc.cl>
2020-11-08 00:08:36 -0300

    uptade 100

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ api.get('/groups', function (req, res) {
 
 router.get('/', function (req, res) {
   console.log('holaaaa')
-  res.send('hola como esta Updated ultima14');
+  res.send('hola como esta Updated ultima15');
 });
 
 router.get('/world', function (req, res) {
