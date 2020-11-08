b2fbc7f12e665717838b118d74d1e2fe71358dd5
Lucas <ljrodriguez1@uc.cl>
2020-11-08 19:22:20 -0300

    change nginx

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ api.get('/groups', function (req, res) {
 
 router.get('/', function (req, res) {
   console.log('holaaaa')
-  res.send('hola como esta Updated 31');
+  res.send('hola como esta Updated 32');
 });
 
 router.get('/world', function (req, res) {
