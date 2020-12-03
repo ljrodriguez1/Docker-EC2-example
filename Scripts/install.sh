b508d99ec93be5f2bca2b597acb670122f93bbd7
Lucas <ljrodriguez1@uc.cl>
2020-12-03 13:24:13 -0300

    not stoping webserver

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
 pwd=$( aws ecr get-login-password --region us-east-1 )
-sudo docker container stop $(docker container ls -aq)
+sudo docker container stop authjs nodejs
 sudo docker login -u AWS -p $pwd https://132819300562.dkr.ecr.us-east-1.amazonaws.com
 sudo docker pull 132819300562.dkr.ecr.us-east-1.amazonaws.com/arquichatdocker
